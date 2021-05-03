ae2067f2ae1e6584cc04d53295b30c09b4722be8
Yuru Zhou <yuz101@ucsd.edu>
2021-05-03 03:09:02 -0700

    Update the method to concatnate strings of one number together

--- main.js
+++ main.js
@@ -17,11 +17,13 @@ let s_arr = []
 let digit_str = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
 for (let i = 0; i < myArgs.length; i++){
     let currNum = myArgs[i]
+    let num_str = ""
     while(currNum > 0){
         let digit = currNum % 10
         currNum = Math.floor(currNum / 10)
-        s_arr.push(digit_str[digit])
+        num_str += digit_str[digit]
     }
+    s_arr.push(num_str)
     
 }
 console.log(s_arr)
